803d02f5eb9bf721ddb36eb3ae5120e7a3de8918
Michael Frayer <mrfrayer@yahoo.com>
2012-07-08 16:27:17 -0400

    Adding support for Windows based systems with the "getPathToWsAntScript" method which will look for the .sh or .bat form of the "ws_ant" command line script.

--- src/main/groovy/org/frayer/gradle/plugins/websphere/tasks/WsAntWrapperTask.groovy
+++ src/main/groovy/org/frayer/gradle/plugins/websphere/tasks/WsAntWrapperTask.groovy
@@ -8,6 +8,7 @@ import org.frayer.gradle.plugins.utils.PriorityToObjectPropertyPopulator;
 import org.frayer.gradle.plugins.utils.PropertyPopulator
 import org.frayer.gradle.plugins.websphere.WebSphereExtension
 import org.gradle.api.DefaultTask
+import org.gradle.api.InvalidUserDataException;
 import org.gradle.api.tasks.TaskAction;
 
 
@@ -38,8 +39,24 @@ abstract class WsAntWrapperTask extends DefaultTask {
         }
     }
 
+    def getPathToWsAntScript() {
+        def wsAntPathLocation = "${wasHome}/bin"
+        def wsAntFileNamePattern = ~/^ws_ant\.(sh|bat)$/
+
+        def wasAntDirectory = new File(wsAntPathLocation)
+        wasAntDirectory.listFiles().find { it.name ==~ wsAntFileNamePattern }?.absolutePath
+    }
+
     def executeAntScript() {
-        def wsAntProc = "${wasHome}/bin/ws_ant.sh -f ${workingDirectory}/build.xml".execute()
+        def wsAntScriptPath = pathToWsAntScript
+
+        if (!wsAntScriptPath) {
+            def message = "Could not locate the ws_ant.(sh|bat) script needed to run this task. Please check the value provided for 'wasHome'"
+            logger.error(message)
+            throw new InvalidUserDataException(message)
+        }
+
+        def wsAntProc = "${pathToWsAntScript} -f ${workingDirectory}/build.xml".execute()
         wsAntProc.consumeProcessOutput(System.out, System.err)
         wsAntProc.waitFor()
     }
